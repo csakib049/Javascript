6d58986985eb980922449bb461c013d5bfffa5a1
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-06 08:18:56 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,5 +1,64 @@
-asdfsdfsdfsfdf
+// let arr=[1,2,3,4];
 
-sdfsdfsdf
+// function sakib(val){
+//     console.log(val);
+// }
 
-dsasdfsdfsdfsdf
+
+// arr.forEach(sakib);
+
+// let arr=[1,2,3,4];
+
+// function sakib(num){
+//     console.log(num);
+// }
+
+// arr.forEach(sakib);
+
+
+
+// let arr=[1,2,3,4];
+
+// arr.forEach(function sakib(num){
+//     console.log(num);
+// });
+
+
+
+
+// let arra=[2,4,5,6];
+// arra.forEach(function sakib(val){
+//     console.log(val);
+// });
+
+
+
+// let arr=[1,3,4,2,4];
+
+// arr.forEach(function sakib(val){
+//     console.log(val);
+// });
+
+
+
+
+// let arr=[1,3,4,5,6];
+
+// arr.forEach( sakib=(val)=>console.log(val));
+
+
+
+let arr=[1,2,4,5];
+
+let a=arr.filter((m)=> m<10);
+
+
+console.log(a);
+
+
+// let arr=[1,2,3,4];
+
+
+// arr.forEach( sakib=(num)=> console.log(num));
+
+
