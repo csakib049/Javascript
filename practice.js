44394a6e927074d28462fcf155a37d74e19c0132
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-21 19:48:47 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,10 +1,39 @@
-let sakib = document.querySelector(".btn "); 
+let buttons = document.querySelectorAll(".mybtn");
 
 
-sakib.classList.add("enable");
+buttons.classList.add("disabled");
+buttons.classList.remove("mybtn");
 
 
-sakib.addEventListener("click",()=>{
-    sakib.classList.toggle("enable");
-    sakib.classList.toggle("disable");
+// buttons.forEach((btn)=>{
+//     btn.classList.add("enabled");
+// });
+
+// buttons.forEach((btn)=>{
+//     btn.classList.replace("enabled","disabled");
+// });
+
+
+
+//----------------------------------------------------
+
+
+
+// buttons.forEach((btn)=>{
+//     btn.addEventListener("mouseover",()=>{
+//         btn.classList.toggle("hover");
+//     });
+// });
+
+
+// buttons.forEach((b)=>{
+//     b.addEventListener("click",()=>{
+//         b.classList.toggle("hover");
+//     });
+// }); 
+
+buttons.forEach((b) => {
+    b.addEventListener("click", () => {
+        b.classList.add("hover");
+    });
 });
