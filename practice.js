886cf0deaba1296b7fdc8671b77cb2f2c759e257
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-22 05:24:29 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,27 +1,7 @@
-// Simple Promise that works (resolves)
-const goodPromise = new Promise((resolve, reject) => {
-    console.log("Making a good promise...");
-    resolve("Everything is good!");
-});
-
-goodPromise.then((message) => {
-        console.log("Success: " + message);
-    })
-    .catch((error) => {
-        console.log("Error: " + error);
-    });
-
-console.log("-------------------");
-
-// Simple Promise that fails (rejects)
-const badPromise = new Promise((resolve, reject) => {
-    console.log("Making a bad promise...");
-    reject("Something went wrong!");
-});
-
-badPromise.then((message) => {
-        console.log("Success: " + message);
-    })
-    .catch((error) => {
-        console.log("Error: " + error);
-    });
+db.sakib.aggregate([
+    {
+        $match:{
+            name: "shahridfadfddsdfr"
+        }
+    }
+])
