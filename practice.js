a2040771651cf823e69e74ffd65a4a1cb836b619
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-22 15:53:51 +0600

    promise

--- practice.js
+++ practice.js
@@ -1,7 +1,25 @@
-db.sakib.aggregate([
-    {
-        $match:{
-            name: "shahridfadfddsdfr"
-        }
-    }
-])
+const getPromise=()=>{
+  return new Promise((resolve,reject)=>{
+    console.log("I am promise.");
+     resolve("resolve!!!!!!");
+  });
+};
+
+let promise=getPromise();
+promise.then((resolve)=>{
+  console.log("Promise fullfield",resolve);
+});
+
+
+// const sakib=()=>{
+//   return new Promise((resolve,reject)=>{
+//     console.log("Promise message");
+//     resolve("resolve message");
+//   });
+// }
+
+
+// let s=sakib();
+// s.then((resolve)=>{
+//   console.log("hi this is sakib ",resolve);
+// });
