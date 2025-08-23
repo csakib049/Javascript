a297fe21412fb1b04fcac44de75afc6eb2f4f7f0
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-23 20:17:47 +0600

    update

--- practice.js
+++ practice.js
@@ -3,7 +3,7 @@ const url="https://jsonplaceholder.typicode.com/users";
 const getfacts=async()=>{
     console.log("getting data .................");
     let response = await fetch(url);
-    console.log(responce); // JSON format
+    console.log(response); // JSON format
     let data = await response.json();
     console.log(data); 
 };
