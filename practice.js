20d9813b935c148756d17b85c396566fef999678
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-24 20:10:59 +0600

    commit

--- practice.js
+++ practice.js
@@ -1,9 +1,30 @@
-const url="https://jsonplaceholder.typicode.com/users";
+// const url="https://jsonplaceholder.typicode.com/users";
 
-const getfacts=async()=>{
-    console.log("getting data .................");
-    let response = await fetch(url);
-    console.log(response); // JSON format
-    let data = await response.json();
-    console.log(data); 
+// const getfacts=async()=>{
+//     console.log("getting data .................");
+//     let response = await fetch(url);
+//     console.log(response); // JSON format
+//     let data = await response.json();
+//     console.log(data); 
+// };
+
+
+// getfacts();
+
+
+
+const url= "    ";
+
+const getdata=()={
+    let response= await fetch(url);
+    console.log(response);
+
+    let data =await Response.json();
+    console.log(data);
+
 };
+
+
+getdata();
+
+
