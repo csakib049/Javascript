1a50faa2706d085dc2ed609a9b90197a933d5708
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-28 20:28:05 +0600

    Practice

---
+++ practice.js
@@ -0,0 +1,14 @@
+// let h1=document.getElementById("h1");
+
+// h1.innerHTML="sakdifbalksdfjas;dlkfj";
+
+
+
+// let p1=document.getElementsByClassName("p1")[0];
+
+// p1.innerHTML="kire kaj korse";
+
+
+// let h2=document.getElementsByTagName("h2")[0];
+
+// h2.innerHTML="wsdfsdfsdf";
