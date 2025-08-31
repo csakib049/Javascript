55fe286956dd59a7ba29af2f9586a05ba38d5646
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-08-31 06:03:45 +0600

    practice

---
+++ practice.js
@@ -0,0 +1,19 @@
+// let para=document.querySelector(".para");
+
+
+// let a=para.getAttribute("class");
+// console.log(a);
+
+
+// para.setAttribute("class","sakib");
+// let b=para.getAttribute("class");
+// console.log(b);
+
+
+let para=document.querySelector("p");
+
+console.log(para.getAttribute("class"));
+
+para.setAttribute("class","sakib");
+
+console.log(para.getAttribute("class"));
