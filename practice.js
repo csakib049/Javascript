6b506d6add5055cbf4451110b404fc2348014141
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-09-05 21:55:50 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,5 +1,17 @@
-for (code in countryList){
-  console.log(code);
+for( let select of dropdowns){
+  for(currcode in countryList){
+    let newOption = document.createElement("option");
+    
+    newOption.innerText=currcode;
+    newOption.value=currcode;
+    
+
+    if(select.name=="from" && currCode == "USD"){
+      newOption.selected="selected";
+    }
+
+    select.append(newOption);
+  }
 }
 
 
