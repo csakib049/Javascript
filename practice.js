845c4e3b5bd96d532037070343c24669c29731b3
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-09-05 20:01:14 +0600

    practice

--- practice.js
+++ practice.js
@@ -1,24 +1,3 @@
-const url = "https://api.thecatapi.com/v1/images/search";
-
-let factpara = document.querySelector("#fact");
-
-let button=document.querySelector("#button");
-
-
-
-const getfacts = async () => {
-  console.log("Fetching data...");
-  let response = await fetch(url);
-  let data = await response.json();
-  
-  console.log(data); // check the array in console
-
-  // Since API returns an array, take the first element
-  factpara.innerText = data[0].url;
-};
-
-
-
-
-button.addEventListener("click",getfacts);
-
+for (code in countryList){
+  console.log(code);
+}
