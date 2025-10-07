57ae213f221fcab7f12dcd9083b2057e5bfc3d32
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-10-07 10:53:42 +0600

    Digital_Clock

--- typescript.js
+++ typescript.js
@@ -1,2 +1,18 @@
-var nam = "sakib";
-console.log(nam);
+// var userdata={
+//     name:string,
+//     id:number,
+//     company:string
+// }={
+//     name:"sakib",
+//     id:234,
+//     company:"samsung"
+// }
+var _a;
+var userdata = (_a = {
+    name: "sakib",
+    id: 390,
+    company: "samsung"
+}, string = _a.name, number = _a.id, string = _a.company, _a);
+userdata.name = "shahriar";
+console.log(userdata.name);
+console.log(userdata.company);
--- typescript.ts
+++ typescript.ts
@@ -1,2 +1,25 @@
-let nam:string="sakib";
-console.log(nam);
+// var userdata={
+//     name:string,
+//     id:number,
+//     company:string
+// }={
+//     name:"sakib",
+//     id:234,
+//     company:"samsung"
+// }
+
+var userdata={
+    name:string,
+    id:number,
+    company:string
+}={
+    name:"sakib",
+    id:390,
+    company:"samsung"
+}
+
+
+userdata.name="shahriar";
+
+console.log(userdata.name);
+console.log(userdata.company);
