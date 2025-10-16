511a3d0257f245ee2a055a127a9b9f6001a80123
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-10-16 16:16:01 +0600

    first react project

---
+++ ui-design/src/components/Section1/RightContent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import RightCard from './RightCard'
+
+const RightContent = () => {
+  return (
+    <div className='h-full w-2/3 p-6 flex flex-nowrap gap-10 overflow-x-auto '>
+      <RightCard/>
+      <RightCard/>
+      <RightCard/>
+      <RightCard/>
+      <RightCard/>
+      <RightCard/>
+    </div>
+  )
+}
+
+export default RightContent
+
+
+
