ddd1c97caeb3ce9ae10332654464186c55e7bef4
Md.Sakib Chowdhury <csakib049@gmail.com>
2025-10-28 21:22:23 +0600

    nested route

---
+++ React Practice/practice-project/src/pages/Men.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Men = () => {
+  return (
+    <div>
+      <h1 className='bg-blue-500 text-white underline font-bold '>This is a maaaaaaans World ! </h1>
+    </div>
+  )
+}
+
+export default Men
---
+++ React Practice/practice-project/src/pages/Product.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+const Product = () => {
+  return (
+    <div>
+      <div className='flex justify-center gap-10 py-4'>
+        <Link className='text-xl font-bold text-orange-500' to='/product/men'>Men</Link>
+        <Link className='text-xl font-bold text-orange-500' to='/product/women'>Women</Link>
+      </div>
+         
+         
+
+      <Outlet/>
+    </div>
+  )
+}
+
+export default Product
